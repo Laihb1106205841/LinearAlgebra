87cd4b4ab09a7baeeca4d0c2edc1a06801fb8337
赖海斌 <12211612@mail.sustech.edu.cn>
2023-02-14 20:34:12 +0800

    first draft

--- src/lesson1/ERO.java
+++ src/lesson1/ERO.java
@@ -45,6 +45,7 @@ public class ERO {
         NSolution =STMatrix(Matrix);
 
 
+
         for(int i=0;i<row;i++){
             for(int j=0;j<col;j++){
                 System.out.print(NSolution[i][j]+" ");
